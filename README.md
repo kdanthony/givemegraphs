b4047cc64203c378d9e0a82c2a43613de6b3c970
root <root@zoidberg.local>
2013-12-22 02:50:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Give-Me-Graphs
+==============
+
+Graphs the Give-Me-Coins API into Carbon/Graphite
+
+To use this, create a config.php file and fill it with the following (change the values as needed):
+
+<?
+
+$apiurl = 'https://give-me-coins.com/pool/api-ltc?api_key=??????????';
+$carbon_host = '127.0.0.1';
+$carbon_port = 2003;
+
+?>
---
+++ givemegraphs.php
@@ -0,0 +1,69 @@
+<?php
+
+# Give-Me-Coins API Graphite Graphing
+# ================================================================================
+# Reads in the API for Give-Me-Coins and stuffs worker stats into Carbon so they
+# can be graphed in Graphite.
+# ================================================================================
+#
+# Copyright (c) 2013, Kevin Anthony (kevin@anthonynet.org)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.A
+
+include('config.php');
+
+$api_result = file_get_contents( $apiurl );
+$api_parsed = json_decode( $api_result, true);
+
+$total_hashrate = 0;
+$timestamp = time();
+
+$carbon = fsockopen( $carbon_host, $carbon_port, $error_no, $error_str, 1);
+
+if ( $carbon ) {
+
+    foreach ( $api_parsed['workers'] as $worker ) {
+
+        $total_hashrate = $total_hashrate + $worker['hashrate'];
+        $share_delta = $timestamp - $worker['last_share_timestamp'];
+
+        $carbon_message = 'mining.litecoin.' . $worker['username'] . '.hashrate ' . $worker['hashrate'] . ' ' .  $timestamp . "\n";
+        fwrite( $carbon, $carbon_message );
+
+        $carbon_message = 'mining.litecoin.' . $worker['username'] . '.sharedelta ' . $share_delta . ' ' .  $timestamp . "\n";
+        fwrite( $carbon, $carbon_message );
+
+    }
+
+    $carbon_message = 'mining.litecoin.totals.hashrate' . ' ' . $total_hashrate . ' ' .  $timestamp . "\n";
+    fwrite( $carbon, $carbon_message );
+
+} else {
+    print "Error: $error_str ($error_no) \n";
+    exit(1);
+}
+
+fclose( $carbon );
+
+?>
+
+
